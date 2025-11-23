59ca7afbae829dedba290c5f0d4f05b9cfd0e22d
Tyler <tch87@cornell.edu>
2025-11-22 21:35:25 -0500

    network manager set up

--- luminary-frontend/Networking/NetworkManager.swift
+++ luminary-frontend/Networking/NetworkManager.swift
@@ -5,4 +5,36 @@
 //  Created by Tyler on 11/22/25.
 //
 
+import Alamofire
 import Foundation
+
+class NetworkManager {
+
+    /// Shared singleton instance
+    static let shared = NetworkManager()
+
+    private init() { }
+
+    // MARK: - Requests
+    
+    func fetchPosts(completion: @escaping (Result<[Post], Error>) -> Void) {
+        let endpoint = "placeholder"
+
+        let decoder = JSONDecoder()
+
+        // Create the request
+        AF.request(endpoint, method: .get)
+            .validate()
+            .responseDecodable(of: [Post].self, decoder: decoder) { response in
+                // Handle the response
+                switch response.result {
+                case .success(let posts):
+                    print("Successfully fetched \(posts.count) posts")
+                    completion(.success(posts))
+                case .failure(let error):
+                    print("Error in NetworkManager.fetchPosts: \(error.localizedDescription)")
+                    completion(.failure(error))
+                }
+            }
+    }
+}
