e4b26fa9a2980b3ceba31b39c0c845e66ddbd82a
Tyler <tch87@cornell.edu>
2025-11-22 21:32:14 -0500

    added post struct

--- luminary-frontend/Models/Post.swift
+++ luminary-frontend/Models/Post.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Post: Hashable, Codable {
+    let displayName: String
+    let postTime: Date
+    let message: String
+    let constelationName: String
+    let postType: String
+}
+
+// this is temporary
+var dummyData = [
+    Post(displayName: "Kalyee", postTime: Date(), message: "bluh", constelationName: "lil dipper", postType: "star"),
+    Post(displayName: "Tyler", postTime: Date(), message: "hi", constelationName: "big dipper", postType: "constellation")
+]
