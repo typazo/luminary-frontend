88a4031845b10efee26d4a770d1dd267dc183dee
Tyler <tch87@cornell.edu>
2025-11-22 20:49:52 -0500

    Initial commit

---
+++ luminary-frontend/luminary_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  luminary_frontendApp.swift
+//  luminary-frontend
+//
+//  Created by Tyler on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct luminary_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ luminary-frontendUITests/luminary_frontendUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  luminary_frontendUITestsLaunchTests.swift
+//  luminary-frontendUITests
+//
+//  Created by Tyler on 11/22/25.
+//
+
+import XCTest
+
+final class luminary_frontendUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
