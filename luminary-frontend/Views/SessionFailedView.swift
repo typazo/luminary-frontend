264cdc57f9938446bd23dd744d7ebe21467df0fd
ukaylee <kaylee.ulep@gmail.com>
2025-12-05 01:21:25 -0500

    feat: finished ui for the failed session screen

--- luminary-frontend/Views/SessionFailedView.swift
+++ luminary-frontend/Views/SessionFailedView.swift
@@ -12,17 +12,57 @@ struct SessionFailedView: View {
     let onReturnToStart: () -> Void
 
     var body: some View {
-        VStack(spacing: 16) {
-            Text("Your session failed so your star faded")
-                .font(.title)
-            Button("Return to Start") {
-                onReturnToStart()
+        ZStack(){
+            Image("star_bg")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea() // covers entire screen
+         
+            VStack(spacing: 16) {
+                
+    //            Text("Your session failed so your star faded")
+    //                .font(.title)
+                Image("faded_text")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: UIScreen.main.bounds.width)
+                    .padding(.top, 120)
+                    
+                
+                Spacer()
+                
+                Image("star_fade")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:250)
+            
+                Spacer()
+                
+                Button(action: {
+                    onReturnToStart()
+                }) {
+                    Text("Try Again")
+                        .font(.custom("CormorantInfant-SemiBold", size: 20))
+                        .foregroundColor(.warmPurple)
+                        .padding()
+                        .frame(maxWidth: 216)
+                        .background(Color.veryLightPurple)
+                        .cornerRadius(8)
+                        
+                }
             }
+            .padding(.bottom, 180)
             .padding()
-            .background(Color.red.opacity(0.2))
-            .cornerRadius(8)
+            .toolbar(.hidden, for: .tabBar)
         }
-        .padding()
-        .toolbar(.hidden, for: .tabBar)
+        
+    }
+        
+}
+
+
+struct SessionFailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        SessionFailedView(onReturnToStart: {print("Return to start tapped (placeholder)")})
     }
 }
