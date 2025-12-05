d0cef2e6fb601eb57c811fea8478a0b0ca27290e
ukaylee <kaylee.ulep@gmail.com>
2025-12-04 23:12:16 -0500

    profile page (no constellations) ui implemented

--- luminary-frontend/Views/ProfileView.swift
+++ luminary-frontend/Views/ProfileView.swift
@@ -11,60 +11,144 @@ import SwiftUI
 struct ProfileView : View {
     @StateObject var viewModel = ProfileViewModel()
     @EnvironmentObject var settings: UserSettings
-
+    
     var body : some View {
-        ScrollView{
-            LazyVStack(spacing: 16){
-                Image(systemName: "globe")
+        GeometryReader { fullGeo in
+            ZStack{
+                Image("star_bg")
                     .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
+                    .scaledToFill()
+                    .ignoresSafeArea() // covers entire screen
                 
-                //later ill just have one text that has different words based on if with a text variable
-                if let name = settings.displayName {
-                    Text("\(name)'s Galaxy :)")
-                        .font(.system(size: 24))
-//                } else {
-//                    Text("Someone's Galaxy :)")
-//                        .font(.system(size: 24))
-                }
-                if let stats = viewModel.userStats {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("Stars completed: \(stats.starsCompleted)")
-                        Text("Constellations Completed: \(stats.constellationsCompleted)")
-                        Text("Hours Studied: \(stats.hoursStudied)")
+                GeometryReader { contentGeo in
+                    ScrollView{
+                        LazyVStack(spacing: 16){
+                            Image("sample_pfp")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 117, height: 117)
+                            //                                .padding(.top,0)
+                            
+                            if let name = settings.displayName {
+                                Text("\(name)'s galaxy")
+                                    .font(.custom("CormorantInfant-Bold", size: 35))
+                                    .foregroundColor(Color.warmPurple)
+                                //                        } else {
+                                //                            Text("Someone's Galaxy :)")
+                                //                                                .font(.system(size: 24))
+                            }
+                            
+                            
+                            // MARK: -- STATS
+                            if let stats = viewModel.userStats {
+                                VStack(alignment: .leading, spacing:10){
+                                    
+                                    HStack(spacing: 8){
+                                        // --- Col1: numbers ---
+                                        VStack(alignment: .trailing){
+                                            Text("\(stats.hoursStudied)")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 19))
+                                                .frame(width: 30, alignment: .trailing)
+                                                .foregroundColor(Color.amour)
+                                            Text("\(stats.starsCompleted)")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 19))
+                                                .frame(width: 30, alignment: .trailing)
+                                                .foregroundColor(Color.amour)
+                                            Text("\(stats.constellationsCompleted)")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 19))
+                                                .frame(width: 30, alignment: .trailing)
+                                                .foregroundColor(Color.amour)
+                                        }
+                                        
+                                        // --- Col2: dots ---
+                                        VStack(){
+                                            Text("•")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 19))
+                                                .foregroundColor(Color.amour)
+                                            Text("•")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 19))
+                                                .foregroundColor(Color.amour)
+                                            Text("•")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 19))
+                                                .foregroundColor(Color.amour)
+                                        }
+                                        
+                                        // --- Col3: text ---
+                                        VStack(alignment: .leading){
+                                            Text("hours completed")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 19))
+                                                .foregroundColor(Color.warmPurple)
+                                            Text("stars completed")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 19))
+                                                .foregroundColor(Color.warmPurple)
+                                            Text("constellations completed")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 19))
+                                                .foregroundColor(Color.warmPurple)
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                            
+                            VStack(spacing: 0){
+                                Rectangle()
+                                    .fill(Color.veryLightPurple)
+                                    .frame(height: 6)
+                                    .edgesIgnoringSafeArea(.horizontal)
+                                
+                                ZStack{
+                                    
+                                    let topContentHeight = contentGeo.frame(in: .local).minY
+                                    let screenHeight = fullGeo.size.height
+                                    
+                                    // The minimum height is the full screen height minus the height of the content above.
+                                    let minConstellationHeight = screenHeight - topContentHeight
+                                    
+                                    // Background Rectangle (Layer 1)
+                                    Rectangle()
+                                        .fill(Color.mediumOrchid)
+                                        .opacity(0.63)
+                                        .frame(maxWidth: .infinity)
+                                    // Apply the calculated minimum height
+                                        .frame(minHeight: minConstellationHeight)
+                                    
+                                    VStack{
+                                        if viewModel.completedConstellations.isEmpty {
+                                            Text("no constellations \nachieved yet")
+                                                .font(.custom("CormorantInfant-SemiBold", size: 30))
+                                                .foregroundColor(Color.amour)
+                                                .multilineTextAlignment(.center)
+                                            //                                    Spacer()
+                                        } else {
+                                            ForEach(viewModel.completedConstellations, id: \.self) { constellation in
+                                                ConstellationCell(constellation: constellation)
+                                                    .padding(.horizontal)
+                                            }
+                                            //                                    Spacer()
+                                        }
+                                        Spacer()
+                                    }
+                                    .padding(.top, 20)
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                                }
+                            }
+                            .padding(.vertical)
+                            Color.clear
+                        }
+                        .refreshable {
+                            if let userId = settings.userId {
+                                await viewModel.fetchProfileData(for: userId)
+                            }
+                        }
+                        .task {
+                            if let userId = settings.userId {
+                                await viewModel.fetchProfileData(for: userId)
+                            }
+                        }
                     }
-                    .font(.headline)
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(12)
-                    .padding(.horizontal)
+                    
                 }
-                if viewModel.completedConstellations.isEmpty {
-                    Text("No completed constellations yet!")
-                        .foregroundColor(.gray)
-                        .padding()
-                } else {
-                    ForEach(viewModel.completedConstellations, id: \.self) { constellation in
-                        ConstellationCell(constellation: constellation)
-                            .padding(.horizontal)
-                    }
-                }
-
-            }
-            .padding(.vertical)
-        }
-        .refreshable {
-            if let userId = settings.userId {
-                await viewModel.fetchProfileData(for: userId)
             }
         }
-        .task {
-            if let userId = settings.userId {
-                await viewModel.fetchProfileData(for: userId)
-            }
-        }
-
     }
 }
-
